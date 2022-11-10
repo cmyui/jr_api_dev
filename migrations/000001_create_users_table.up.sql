8c101b3f64b6b572289ca8d49ce6bedc70767f93
cmyui <cmyuiosu@gmail.com>
2022-11-09 23:53:54 -0500

    feat: migrations for users table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
