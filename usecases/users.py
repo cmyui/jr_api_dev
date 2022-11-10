3d93218e6e91e3477aa1dd30f1ab3a5fa28004c7
cmyui <cmyuiosu@gmail.com>
2022-11-09 23:56:24 -0500

    fix: return user row from create_user usecase

--- usecases/users.py
+++ usecases/users.py
@@ -13,7 +13,17 @@ async def create_user(email: str, first_name: str, password: str):
         "password": utils.security.hash_password(password),
     }
     user_id = await services.database.execute(query, values)
-    return user_id
+
+    query = """
+        SELECT id, email, first_name
+          FROM users
+         WHERE id = :user_id
+    """
+    values = {
+        "user_id": user_id,
+    }
+    user_row = await services.database.fetch_one(query, values)
+    return user_row
 
 
 async def get_user(user_id: int):
